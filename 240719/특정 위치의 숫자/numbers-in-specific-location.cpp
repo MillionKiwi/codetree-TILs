d47bd3e279c5317fec61a31f552939fd4b49340c
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2024-07-19 00:16:34 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0;
+    int a[10];
+    for(int i=0; i < 10; i++){
+        cin >> a[i];
+    }
+    sum = a[2] + a[4] + a[9];
+    cout << sum;
+    return 0;
+}
