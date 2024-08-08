839c04380c2e7bd06b2aa9f87c30d6cdb1e1de88
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2024-08-08 14:06:13 +0900

    Title: 사는 지역, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/사는 지역/where-live.java
+++ 240808/사는 지역/where-live.java
@@ -1,36 +1,45 @@
 import java.util.Scanner;
 
-class Address {
+class people{
+
     String name;
-    String ad;
+    String number;
     String city;
 
-    public Address(String name, String ad,String city){
+    public people(String name,String number,String city){
         this.name = name;
-        this.ad = ad;
+        this.number = number;
         this.city = city;
     }
 }
 
+
+
 public class Main {
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        people[] word = new people[n];
 
-    int n = sc.nextInt();
-    
-    Address[] ads = {
-        new Address(sc.next(), sc.next(), sc.next()),
-        new Address(sc.next(), sc.next(), sc.next()),
-        new Address(sc.next(), sc.next(), sc.next())
-    }; 
-    int minAds = 0;
-    for(int i = 0; i < n; i++){
-        if (ads[i].name.compareTo(ads[minAds].name) > 0)  minAds = i;
-    }
-    System.out.println("name " + ads[minAds].name);
-    System.out.println("addr " + ads[minAds].ad);
-    System.out.println("city " + ads[minAds].city);
-    }
+        for(int i = 0 ; i<n; i++){
+            String name = sc.next();
+            String number = sc.next();
+            String city = sc.next();
 
+            word[i] = new people(name,number,city);
+        }
+
+        int min_val = 0;
+
+        for(int i = 1; i<n; i++){
+            if (word[i].name.compareTo(word[min_val].name)>0){
+                min_val = i;
+            }
+        }
+        System.out.println("name "+word[min_val].name);
+        System.out.println("addr "+word[min_val].number);
+        System.out.println("city "+word[min_val].city);
+    }
 }
