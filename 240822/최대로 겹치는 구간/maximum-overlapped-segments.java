153f08f22938ea39944cd119d0f9876294196a28
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2024-08-22 23:20:00 +0900

    Title: 최대로 겹치는 구간, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240822/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240822/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
          Scanner sc = new Scanner(System.in);
          int n = sc.nextInt();
-         int offset = 100;
+         int offset = 101;
 
          int[] blocks = new int[250];
 
