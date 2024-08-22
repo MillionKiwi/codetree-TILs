d8060c4236d0083c2ee66d1e01a96eee4e26e7c7
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2024-08-23 00:10:45 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240823/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int max = 2000;
+        int[] blocks = new int[max];
+
+        for(int i = 0; i < max; i++) blocks[i] = 0;
+
+        int n = sc.nextInt();
+        int now = max / 2;
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            String dir = sc.next();
+
+            if(dir.equals("R")){
+               for(int j = now; j < now + x - 1; j++){
+                  blocks[j]++;
+               }
+            }
+            if(dir.equals("L")){
+               for(int j = now; j > now - x; j--){
+                  blocks[j]++;
+               }
+            }
+        }
+
+        for(int i = 0; i < max; i++){
+         if(blocks[i] > 1){
+            cnt++;
+         }
+        }
+
+        System.out.println(cnt);
+    }
+}
