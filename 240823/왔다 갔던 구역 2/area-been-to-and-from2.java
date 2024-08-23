9bef770bf993703aadbaf360405d7e7ebe405ee8
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2024-08-23 22:50:35 +0900

    Title: 왔다 갔던 구역 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240823/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240823/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int max = 25;
+        int max = 1000;
         int[] blocks = new int[max];
 
         for(int i = 0; i < max; i++) blocks[i] = 0;
